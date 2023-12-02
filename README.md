f31f62fe5d777d0bb02a9a2f7332576f655d80be
Conor Golden <conorgolden1@hotmail.com>
2023-12-02 14:36:59 -0500

    Init Commit: Added readme and completed templating

---
+++ README.md
@@ -0,0 +1,12 @@
+# JS Advent of Code 2023
+This is my personal advent of code 2023 repo. I used a templating system to streamline redundant boilerplate. The templating system could be abstracted to setup the system for different languages. Another consideration is implenting a webscraper with the necessary account authentication to pull the necessary data down into a file to further reduce redundencies. I will be putting my solutions in the solutions branch and keeping the templating here in master.
+## Requirements
+1. Nodejs
+2. Jest
+## Instructions
+1. Clone the repo
+2. Run:
+   chmod +x create_day.sh
+3. For each indiviudal day:
+    bash create_day.sh
+
---
+++ create_day.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$#" -ne 0 ]; then
+    echo "Usage: this script takes no commandline parameters"
+    exit 1
+fi
+
+read -p "Enter the advent of code day (1-25): " user_input
+
+if [[ ! ( "$user_input" =~ ^[0-9]+$ && "$user_input" -ge 1 && "$user_input" -le 25) ]]; then
+    echo "Error: $user_input is invalid"
+    exit 1
+fi
+
+template_folder="template/"
+
+if [ ! -d "$template_folder" ]; then
+    echo "Error: Template folder not found"
+    exit 1
+fi
+
+dest_folder="day_$user_input"
+
+if [ ! -d "$dest_folder" ]; then
+    mkdir -p "$dest_folder"
+fi
+
+find "$template_folder" -type f | while read -r file; do
+    if [[ "$file" =~ ^"template/template" ]]; then
+        new_file=$(echo $file | sed "s#template/template\\.#$dest_folder.#")
+        cp "$file" "$dest_folder/$new_file"
+    else
+        new_file=$(echo $file | sed "s#template/##")
+        cp "$file" "$dest_folder/$newfile"
+    fi
+done
+
+echo "Files copied and renamed successfully from $template_folder to $dest_folder"
