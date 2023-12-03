94cd00138e6e2a89b7d67f888cf2878e972086e6
Conor Golden <conorgolden1@hotmail.com>
2023-12-03 02:26:42 -0500

    finished day_2 part2

--- day_2/day_2.js
+++ day_2/day_2.js
@@ -41,8 +41,31 @@ function parseGame(line) {
     return line.split(':')[1].split(';').map((line) => { return line.split(',') });
 }
 
-function part2(data) {
+function maxSet(set, maxGame) {
+    const newGame = maxGame;
+    for (entry of set) {
+        const cubes = entry.substr(1).split(' ')
+        const color = cubes[1]
+        const numCubes = Number(cubes[0])
+        if (newGame[color] < numCubes) {
+            newGame[color] = numCubes
+        }
+    }
+    return newGame
 
+}
+function part2(data) {
+    const lines = data.split("\n")
+    const games = lines.map((line) => { return parseGame(line) })
+    const powerSets = []
+    for (i in games) {
+        let maxGame = { 'red': 0, 'blue': 0, 'green': 0 }
+        for (set of games[i]) {
+            maxGame = maxSet(set, maxGame)
+        }
+        powerSets.push(Object.values(maxGame).reduce((acc, n) => { return acc * n }, 1))
+    }
+    return powerSets.reduce((acc, n) => { return acc + n }, 0)
 }
 
 module.exports = {
--- day_2/day_2.test.js
+++ day_2/day_2.test.js
@@ -16,7 +16,7 @@ test(`day ${day} basic part 2`, () => {
 // Update below fields to reflect test data
 const part1Result = 8
 
-const part2Result = undefined
+const part2Result = 2286
 
 const testData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -24,4 +24,4 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
-const testData2 = undefined
+const testData2 = testData
