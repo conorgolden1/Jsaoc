29608b26ac860beabbf59ccd1f344e9e7ecae49d
Conor Golden <conorgolden1@hotmail.com>
2023-12-02 22:30:26 -0500

    Added data

--- day_1/day_1.test.js
+++ day_1/day_1.test.js
@@ -7,14 +7,17 @@ const { part1, part2 } = require(`./day_${day}.js`)
 test(`day ${day} basic part 1`, () => {
     expect(part1(test_data)).toBe(part1Result)
 });
-test(`day ${day} basic part 1`, () => {
+test(`day ${day} basic part 2`, () => {
     expect(part2(test_data)).toBe(part2Result)
 });
 
 
 // Update below fields to reflect test data
-const part1Result = None
+const part1Result = 142
 
 const part2Result = None
 
-const test_data = None
+const test_data = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
