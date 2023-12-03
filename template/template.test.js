c2b08b5e7b191362f3a14d35c2ad8f042dba90a8
Conor Golden <conorgolden1@hotmail.com>
2023-12-03 01:00:19 -0500

    updated templating for main file | fixed typos

--- template/template.test.js
+++ template/template.test.js
@@ -6,14 +6,17 @@ const { part1, part2 } = require(`./day_${day}.js`)
 test(`day ${day} basic part 1`, () => {
     expect(part1(test_data)).toBe(part1Result)
 });
+
 test(`day ${day} basic part 2`, () => {
     expect(part2(test_data)).toBe(part2Result)
 });
 
 
 // Update below fields to reflect test data
-const part1Result = None
+const part1Result = undefined
 
-const part2Result = None
+const part2Result = undefined
 
-const test_data = None
+const testData = undefined
+
+const testData2 = undefined
