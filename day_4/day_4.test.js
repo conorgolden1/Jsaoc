fd26f460640adcfb423040ba20c4c56e52074871
Conor Golden <conorgolden1@hotmail.com>
2023-12-03 23:55:11 -0500

    day_4 init

---
+++ day_4/day_4.test.js
@@ -0,0 +1,23 @@
+const day = 4
+
+const { test, expect } = require('@jest/globals')
+const { part1, part2 } = require(`./day_${day}.js`)
+
+
+test(`day ${day} basic part 1`, () => {
+    expect(part1(testData)).toBe(part1Result)
+});
+
+test(`day ${day} basic part 2`, () => {
+    expect(part2(testData2)).toBe(part2Result)
+});
+
+
+// Update below fields to reflect test data
+const part1Result = undefined
+
+const part2Result = undefined
+
+const testData = undefined
+
+const testData2 = undefined
