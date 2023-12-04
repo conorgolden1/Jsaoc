7c0d5459ffc5e59b32737a86ef1bf3d120e566cc
Conor Golden <conorgolden1@hotmail.com>
2023-12-04 00:23:49 -0500

    day_4 part 1 finished

--- day_4/day_4.js
+++ day_4/day_4.js
@@ -1,9 +1,46 @@
 const { error } = require('console');
 
 function part1(data) {
+    const lines = data.split("\n")
+    const games = lines.map((line) => { return parseGame(line) })
+    const number = games.map((game) => {
+        if (game[0] === undefined || game[1] === undefined) {
+            return 0
+        }
+        return countWinning(parseNumbers(game[0]), parseNumbers(game[1])) })
+    console.log(number)
+    return number.reduce((acc, n) => { return acc + n }, 0)
 
 }
 
+function parseNumbers(nString) {
+    return nString.split(' ')
+}
+
+function countWinning(winningNumbers, numbersArr) {
+    console.log(winningNumbers, numbersArr)
+    let count = 0
+    let doubler = 0
+    for (candidate of numbersArr) {
+        if (candidate !== '' && winningNumbers.includes(candidate)) {
+            if (doubler == 0) {
+                doubler++
+            } else {
+
+                doubler *= 2
+            }
+        }
+    }
+    console.log(count)
+    return doubler
+}
+
+function parseGame(line) {
+    if (line.split(':')[1] === undefined) {
+        return []
+    }
+    return line.split(':')[1].split('|')
+}
 function part2(data) {
 
 }
