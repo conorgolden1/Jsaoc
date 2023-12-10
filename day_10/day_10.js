2214b3d6b39f0f29f392b2084f873913f69ba4c5
Conor Golden <conorgolden1@hotmail.com>
2023-12-10 16:05:42 -0500

    Finished day 10 part 2

--- day_10/day_10.js
+++ day_10/day_10.js
@@ -6,25 +6,19 @@ function part1(data) {
     const lines = data.split("\n")
     const start = findStart(lines)
     let p = chooseStartDirection(lines, start)
-    let prev_p = start
+    let prev_p = [start[0] - p[0], start[1] - p[1]]
     for (let count = 1; true; count++) {
         const dirs = scanPoint(lines, p);
         if (dirs.length === 0) {
             return count / 2;
         }
-
         [p, prev_p] = chooseDir(dirs, p, prev_p)
-
-
-/*         console.log(dirs);
-        console.log(`P: ${p[0]}, ${p[1]} | Pre: ${p[0] + prev_p[0]}, ${p[1] + prev_p[1]}`); */
     }
-
 }
 
 function chooseDir(dirs, p, prev_p) {
     for (const dir of dirs) {
-        if (prev_p === null || !(prev_p[0] === dir[0] && prev_p[1] === dir[1])) {
+        if (!(prev_p[0] === dir[0] && prev_p[1] === dir[1])) {
             return [[p[0] + dir[0], p[1] + dir[1]], [parseInt(dir[0] * -1), parseInt(dir[1] * -1)]]
         }
     }
@@ -90,8 +84,76 @@ function findStart(data) {
     throw Error("Did not find start")
 }
 
-function part2(data) {
+function replaceS(data, p) {
+    const lines = data;
+    const symbols = []
+    for (c of check) {
+        const sym = data[p[0] + c[0]][p[1] + c[1]]
+        if (typeof sym !== 'undefined' && sym !== '.') {
+            const pos_dir = startSymbols[sym]
+            if (c[0] === pos_dir[0][0] && c[1] === pos_dir[0][1]) {
+                symbols.push([parseInt(c[0] * -1), parseInt(c[1] * -1)])
+            } else if (c[0] === pos_dir[1][0] && c[1] === pos_dir[1][1]) {
+                symbols.push([parseInt(c[0] * -1), parseInt(c[1] * -1)])
+            }
+        }
+    }
+    const str1 = symbols.toString()
+    const str2 = [symbols[1] + symbols[0]].toString()
+    for (const key of Object.keys(startSymbols)) {
+        const keystr = startSymbols[key].toString();
+        if (keystr === str1 || keystr === str2) {
+            lines[p[0]] = lines[p[0]].replace('S', key)
+            break
+        }
+    }
+    return lines
+}
 
+const walls = ['F', '7', '|']
+
+function countEnclosed(line, points, row) {
+    let boundry_count = 0
+    let enclosed = 0
+    for (i in line) {
+        let isPoint = false
+        for (p of points) {
+            if (p[0] === row && p[1] === parseInt(i)) {
+                isPoint = true
+                break
+            }
+        }
+        if (isPoint) {
+            if (walls.includes(line[i])) {
+                boundry_count++
+            }
+        } else if (boundry_count % 2 == 1) {
+            enclosed++
+        }
+    }
+    return enclosed
+}
+
+
+function part2(data) {
+    const lines = data.split("\n")
+    const start = findStart(lines)
+    let p = chooseStartDirection(lines, start)
+    let prev_p = [start[0] - p[0], start[1] - p[1]]
+    let dirs = scanPoint(lines, p);
+    let points = new Array(start)
+    while (dirs.length !== 0) {
+        points.push(p);
+        [p, prev_p] = chooseDir(dirs, p, prev_p);
+        dirs = scanPoint(lines, p);
+    }
+
+    const nLines = replaceS(lines, start)
+    let sum = 0
+    for (i in nLines) {
+        sum += countEnclosed(nLines[i], points, parseInt(i))
+    }
+    return sum
 }
 
 module.exports = {
--- day_10/day_10.test.js
+++ day_10/day_10.test.js
@@ -16,7 +16,7 @@ test(`day ${day} basic part 2`, () => {
 // Update below fields to reflect test data
 const part1Result = 4
 
-const part2Result = undefined
+const part2Result = 8
 
 const testData = `..F..
 .S-7.
@@ -24,4 +24,13 @@ const testData = `..F..
 .L-J.
 .....`
 
-const testData2 = undefined
+const testData2 = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`
