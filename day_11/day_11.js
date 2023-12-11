0c708c9f9f5703457b153ff8b4a130691698cdc1
Conor Golden <conorgolden1@hotmail.com>
2023-12-11 00:58:43 -0500

    day 11 part 1

---
+++ day_11/day_11.js
@@ -0,0 +1,73 @@
+const { error } = require('console');
+const { abs } = require('mathjs');
+
+function part1(data) {
+    /* const lines = expandColumns(expandRows(data.split("\n"))) */
+    const lines = data.split("\n")
+    const hashPoints = findAllHash(lines)
+    return hashPoints.reduce((acc, n) => {
+        return acc + calcDist(n, hashPoints)
+    }, 0) / 2
+}
+
+function calcDist(point, arr) {
+    return arr.reduce((acc, p) => {
+        if (p.toString() === point.toString()) {
+            return acc
+        }
+
+        return acc + abs(point[0] - p[0]) + abs(point[1] - p[1])
+    }, 0)
+}
+
+function expandRows(data) {
+    const newRow = new Array(data[0].length).fill('.', 0, data[0].length)
+    const newData = []
+    for (i in data) {
+        newData.push(data[i])
+        if (!data[i].includes('#')) {
+            newData.push(newRow)
+        }
+    }
+    return newData
+}
+
+function expandColumns(data) {
+    const newData = new Array(data.length).fill('', 0, data.length)
+    for (let x = 0; x < data[0].length; x++) {
+        let foundHash = false
+        for (let y = 0; y < data.length; y++) {
+            if (data[y][x] === '#') {
+                foundHash = true
+            }
+            newData[y] += (data[y][x])
+        }
+        if (!foundHash) {
+            for (let y = 0; y < data.length; y++) {
+                newData[y] += ('.')
+            }
+        }
+    }
+    return newData
+}
+
+function findAllHash(data) {
+    hashPoints = []
+    for (y in data) {
+        for (x in data[y]) {
+            if (data[y][x] === '#') {
+                hashPoints.push([y, x])
+            }
+        }
+    }
+    return hashPoints
+}
+
+function part2(data) {
+
+}
+
+module.exports = {
+    part1,
+    part2
+}
---
+++ day_11/day_11.test.js
@@ -0,0 +1,32 @@
+const day = 11
+
+const { test, expect } = require('@jest/globals')
+const { part1, part2 } = require(`./day_${day}.js`)
+
+
+test(`day ${day} basic part 1`, () => {
+    expect(part1(testData)).toBe(part1Result)
+});
+
+test(`day ${day} basic part 2`, () => {
+    expect(part2(testData2)).toBe(part2Result)
+});
+
+
+// Update below fields to reflect test data
+const part1Result = 374
+
+const part2Result = undefined
+
+const testData = `...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`
+
+const testData2 = undefined
